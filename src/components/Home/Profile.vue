78f3aa3c834c746acf986b51f5f1aa293c789559
adhiyudanas <adhiyudanas@gmail.com>
2019-12-20 09:04:26 +0900

    add link and name card

--- src/components/Home/Profile.vue
+++ src/components/Home/Profile.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="p-profile">
     <main class="p-profile__biodata">
-      <article>
+      <article class="biodata-article">
         <h2>Adhi Yudana Svarajati</h2>
-        <p>Hi! My name is Adhi Yudana Svarajati. You can call me Adhi, and I come from Indonesia</p><br>
+        <p>Hi! My name is Adhi Yudana Svarajati. You can call me Adhi, and I come from Indonesia</p>
         <p>Currently I am residing in Japan and work as a Software Engineer</p>
         <p>I do software development ranging from back-end, database, to front-end.</p>
         <p>Let me tell you something. Although I am still learning, I am quite a good learner!</p><br>
         <p>My company is called Tenkai, in Japanese it's 株式会社tenkai</p>
         <p>It's located in Asakusabashi, Tokyo. Here is the link to my company's website</p>
+        <a @click="imgClicked">10-kai.jp</a>
       </article>
+      <div class="biodata-namecard">
+        <img class="biodata-namecard__element" alt="adhi-name-card" src="../../assets/img/name-card.jpg">
+      </div>
     </main>
     <aside class="p-profile__picture">
       <div class="img-wrapper">
@@ -23,6 +27,8 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 export default class Profile extends Vue {
-
+  public imgClicked () {
+    window.open('https://10-kai.jp', '_blank')
+  }
 }
 </script>
