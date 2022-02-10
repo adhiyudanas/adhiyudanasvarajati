b52759bfcbd808bd346697e436e41d117f04304b
adhiyudanas <adhiyudanas@gmail.com>
2022-02-10 09:52:08 +0900

    fix background

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,22 @@ module.exports = {
     ? '/adhiyudanasvarajati/'
     : '/',
 
+    chainWebpack: (config) => {
+      config
+          .plugin('html')
+          .tap(args => {
+              args[0].title = 'Adhi Yudana Svarajati';
+              args[0].meta = {'theme-color': '#ed7b49'};
+  
+           return args;
+      })
+    },
+
     css: {
         loaderOptions: {
           scss: {
             prependData: `@import "@/styles/main.scss";`,
           },
         },
-    },
+    }
 }
